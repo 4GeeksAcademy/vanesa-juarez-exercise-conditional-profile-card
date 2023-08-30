430a865ae813099de661c7c8e49111b42c39ea09
Vanesa Juarez Paris <131259155+vanesascode@users.noreply.github.com>
2023-08-30 17:55:37 +0000

    social media icons corrected

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,43 @@
 import "../style/index.css";
 
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "left", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
-
-        name: null,
-        lastname: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); //print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+
+  let socialMediaIcons = "";
+  if (
+    variables.twitter !== undefined ||
+    variables.github !== undefined ||
+    variables.linkedin !== undefined ||
+    variables.instagram !== undefined
+  ) {
+    socialMediaIcons = `
+      <ul class=${variables.socialMediaPosition}>
+        ${
+          variables.twitter !== undefined
+            ? `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`
+            : ""
+        }
+        ${
+          variables.github !== undefined
+            ? `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`
+            : ""
+        }
+        ${
+          variables.linkedin !== undefined
+            ? `<li><a href="https://linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`
+            : ""
+        }
+        ${
+          variables.instagram !== undefined
+            ? `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`
+            : ""
+        }
+      </ul>`;
+  }
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
@@ -39,12 +49,7 @@ function render(variables = {}) {
           <h2>${variables.role}</h2>
           <h2>${variables.city}, ${variables.country}</h2>
       
-          <ul class=${variables.socialMediaPosition}>
-            <li><a href="https://twitter.com/${variables.twitter}"  target="_blank"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${variables.github}"  target="_blank"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://www.linkedin.com/${variables.linkedin}"  target="_blank"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://www.instagram.com/${variables.instagram}"  target="_blank"><i class="fab fa-instagram"></i></a></li>
-          </ul>
+          ${socialMediaIcons}
         </div>
     `;
 }
@@ -63,13 +68,13 @@ window.onload = function() {
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
-    twitter: "https://twitter.com",
-    github: "https://github.com/",
-    linkedin: "https://www.linkedin.com",
-    instagram: "https://www.instagram.com",
-    name: "Vanesa",
-    lastname: "Juarez",
-    role: "Developer",
+    twitter: "",
+    github: "",
+    linkedin: "",
+    instagram: "",
+    name: "",
+    lastname: "",
+    role: "",
     country: null,
     city: null
   };
